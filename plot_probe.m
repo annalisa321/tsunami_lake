8e28d69bab6cd290dceb6c63a6107f72d113cfbf
annalisa321 <103445650+annalisa321@users.noreply.github.com>
2022-11-23 17:16:31 +0100

    Update plot_probe.m

--- plot_probe.m
+++ plot_probe.m
@@ -6,16 +6,6 @@ fdir='C:/Users/annal/OneDrive/Desktop/UNIBO/NHWAVE/test_landslide/results/result
 est_ini=574000;
 nord_ini=505600;
 
-est_position1=est1-est_ini;
-est_position2=est2-est_ini;
-est_position3=est3-est_ini;
-
-nord_position1=nord1-nord_ini;
-nord_position2=nord2-nord_ini;
-nord_position3=nord3-nord_ini;
-
-gauge111=[est_position1 est_position2 est_position3, nord_position1 nord_position2 nord_position3 ];
-
 x_land=581078.490;
 y_land=5063591.557;
 
